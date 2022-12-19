c385d11a129ca56780d4895dae158e55a29e1553
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-12-19 16:48:23 +0000

    this will add further styling to the login page

--- Login.jsx
+++ Login.jsx
@@ -7,13 +7,14 @@ import {
   SafeAreaView,
   StyleSheet,
   Pressable,
+  Text,
 } from "react-native";
 import { Formik } from "formik";
 const Login = () => {
   return (
     <SafeAreaView>
       <View>
-        {/* <Image style={styles.logo} source={require("./assets/Barbers.jpg")} /> */}
+        <Image style={styles.logo} source={require("./assets/Barber.png")} />
         <Formik
           initialValues={{ email: "", password: "" }}
           onSubmit={(values) => console.log(values)}
@@ -23,26 +24,31 @@ const Login = () => {
               <TextInput
                 style={styles.username}
                 textAlign={"center"}
-                onChangeText={handleChange("email")}
-                onBlur={handleBlur("email")}
-                value={values.email}
-                placeholder="enter email here"
+                onChangeText={handleChange("username")}
+                onBlur={handleBlur("username")}
+                value={values.username}
+                placeholder="username..."
+                maxLength={10}
+                placeholderTextColor={"white"}
               />
               <TextInput
                 style={styles.password}
                 onChangeText={handleChange("password")}
                 onBlur={handleBlur("password")}
                 value={values.password}
+                placeholderTextColor={"white"}
+                secureTextEntry={true}
+                placeholder="password..."
+                maxLength={10}
               />
-              <Pressable style={styles.login}>
-                <Button
-                  onPress={handleSubmit}
-                  title="Submit"
-                  textAlign={"center"}
-                />
+              {/* <Pressable>
+                <Text style={styles.forgotten} color={"white"} title="Fjnefjnfej?" />
+              </Pressable> */}
+              <Pressable style={styles.login} textAlign={"center"}>
+                <Button color={"black"} onPress={handleSubmit} title="Login" />
               </Pressable>
               <Pressable>
-                <Button title="Not Registered?"></Button>
+                <Button color={"white"} title="Not Registered? Press Here"></Button>
               </Pressable>
             </View>
           )}
@@ -55,28 +61,49 @@ const styles = StyleSheet.create({
   logo: {
     width: 250,
     height: 250,
+    marginBottom: 30,
   },
   username: {
     padding: 5,
     height: 50,
+    width: 240,
     borderWidth: 2,
-    borderRadius: 10,
-    margin: 5,
+    borderRadius: 25,
+    marginBottom: 15,
+    borderColor: "white",
+    textAlign: "center",
+    color: "white",
+    fontSize: 20,
   },
   password: {
     height: 50,
     borderWidth: 2,
-    borderRadius: 10,
-    margin: 5,
+    borderRadius: 25,
+    marginBottom: 15,
+    borderColor: "white",
+    textAlign: "center",
+    color: "white",
+    width: 240,
+    fontSize: 20,
+  },
+  forgotten: {
+    color:"white",
+    backgroundColor:'red'
   },
   login: {
     height: 50,
     borderWidth: 2,
-    borderRadius: 10,
-    margin: 5,
+    borderRadius: 25,
+    marginBottom: 15,
+    borderColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 240,
+    fontSize: 20,
+    backgroundColor: "white",
   },
   registerLink: {
-    margin: 5,
+    margin: 10,
   },
 });
 export default Login;
