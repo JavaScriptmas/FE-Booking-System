9237cf38ea4776976a18541579c7d33a24f49a68
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-12-22 12:11:52 +0000

    this will add the inital code and styling for the appointments page

--- Browse.jsx
+++ Browse.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getAllAppointments } from "./api.js";
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import {
   View,
@@ -10,17 +11,40 @@ import {
   Pressable,
   Text,
   ScrollView,
-} from "react-native";
+} from "react-native"
 import { Formik } from "formik";
 import { postUser } from "./api.js";
+import { format, compareAsc } from 'date-fns';
+import { TimeSlots } from './TimeSlots';
 
 const Browse = ({ navigation }) => {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    getAllAppointments().then((results) => {
+      setAppointments(results);
+      return results;
+    });
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-        <Image style={styles.logo} source={require("./assets/Barber.png")} />
-      <View>
-        <View style={styles.browsecontainer}></View>
-      </View>
+      <Image style={styles.logo} source={require("./assets/Barber.png")} />
+        <ScrollView>
+      <View style={styles.browsecontainer}>
+          {appointments.map((appointment => {
+            return (
+              <View>
+                <Pressable style={styles.id} onPress={() => navigation.navigate("TimeSlots")}>
+              <Text>{format(new Date(appointment._id), 'MM/dd/yyyy')}</Text>
+              <Text>{appointment.count}</Text>
+                </Pressable>
+              </View>
+            );
+          })
+          )}
+          </View>
+          </ScrollView>
     </SafeAreaView>
   );
 };
@@ -31,7 +55,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "black",
-    width: '100%'
+    width: "100%",
   },
   logo: {
     width: 250,
@@ -39,10 +63,24 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   browsecontainer: {
-    width:300,
-    height:440,
-    backgroundColor: 'black',
-  }
+    width: "100%",
+    height: 440,
+    flexWrap: 'wrap',
+    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: "center",
+  },
+  id: {
+    color: "black",
+    textAlign: "center",
+    height: 100,
+    width: 100,
+    backgroundColor: 'green',
+    margin: 10,
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignItems: "center",
+  },
 });
 
 export default Browse;
